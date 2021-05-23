61ae6733254d2fc80fd6f571eaf1512e60c036c8
somnathmistry-app <somnath222666@gmail.com>
2021-05-23 20:57:09 +0530

    cover photo updated commit

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,20 +1,27 @@
+import 'dart:io';
+
+import 'package:Milto/common_components/MyAlertDilog.dart';
+import 'package:Milto/common_components/MySnackBar.dart';
 import 'package:Milto/models/ViewProfileModel.dart';
 import 'package:Milto/views/LoginScreen.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../repos/viewProfile_repo.dart';
 
 class ProfileController extends GetxController {
   var isLoading = true.obs;
   final pref = GetStorage();
-  var profileData = DataProfile();
   String name;
+  DataProfile profileData;
+  var selectedImagePath = ''.obs;
+  var selectedImageSize = ''.obs;
 
   @override
-  void onInit() {
-    //getUserProfileData();
-    requestForUserProfile();
+  void onInit() async{
+    await requestForUserProfile();
+    //await getUserProfileData();
     super.onInit();
   }
 
@@ -25,7 +32,7 @@ class ProfileController extends GetxController {
     super.onReady();
   }
 
-  void requestForUserProfile() async {
+   requestForUserProfile() async {
     try {
       isLoading(true);
       DataProfile response = await viewProfile(pref.read("userId"));
@@ -46,24 +53,58 @@ class ProfileController extends GetxController {
         if (response.personal.address.isNotEmpty) {
           pref.write("city", response.personal.address);
         }
+        if(response.personal.cover.isNotEmpty){
+          pref.write('cover', response.personal.cover);
+        }
       }
     } finally {
       isLoading(false);
     }
   }
 
-  // getUserProfileData() async {
-  //   try {
-  //     isLoading(true);
-  //     DataProfile profileData = await viewProfile(pref.read("userId"));
-  //     if (profileData != null) {
-  //      name = profileData.personal.name;
-  //      print('name ${profileData.personal.name}, namesing: $name');
-  //     }
-  //   } finally {
-  //     isLoading(false);
-  //   }
-  // }
+  void getImage(ImageSource imageSource) async {
+    final pickedFile = await ImagePicker().getImage(source: imageSource);
+    if (pickedFile != null) {
+      selectedImagePath.value = pickedFile.path;
+      selectedImageSize.value =
+          ((File(selectedImagePath.value)).lengthSync() / 1024 / 1024)
+              .toStringAsFixed(2) +
+              'Mb';
+    } else {
+      MySnackbar.infoSnackBar('No Image selected', 'Please select an image');
+    }
+  }
+
+  requestForAddCoverPhoto()async{
+   MyAlertDialog.alertDialog('Cover photo updating..', '');
+    var response = await addCoverPhoto(pref.read("userId"), selectedImagePath);
+
+    // ignore: unrelated_type_equality_checks
+   print('response....... $response');
+    if(response.message=='Successfully Update Cover Picture.'){
+      Get.back();
+      MySnackbar.successSnackBar('Successful', 'Cover Image updated successfully');
+    }else{
+      Get.back();
+      MySnackbar.errorSnackBar('Server Down', 'Server down please try again later');
+    }
+  }
+
+  Future<DataProfile> getUserProfileData() async {
+    try {
+      isLoading(true);
+      profileData = await viewProfile(pref.read("userId"));
+      if (profileData != null) {
+       name = profileData.personal.name;
+       print('name ${profileData.personal.name}, namesing: $name');
+       return profileData;
+      }else{
+        return profileData;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
 
   logOut() async {
     if (pref.read("isLogin") == true) {
--- lib/repos/viewProfile_repo.dart
+++ lib/repos/viewProfile_repo.dart
@@ -9,6 +9,7 @@ import '../models/ViewProfileModel.dart';
 Future<DataProfile> viewProfile(String userId) async {
   final String url = '${GlobalConfiguration().getValue('base_url')}myprofile';
   var client = http.Client();
+  print('............userid: $userId');
 
   var response = await client.post(
     url,
@@ -17,8 +18,32 @@ Future<DataProfile> viewProfile(String userId) async {
   print(response.statusCode);
   if (response.statusCode == 200) {
     String jsonString = response.body;
+    //print('............userid: $userId, response in provider ${responseModelFromJson(jsonString).data.personal.name}');
 
     return responseModelFromJson(jsonString).data;
   } else
     return null;
+}
+
+
+Future<ResponseModel> addCoverPhoto(String userId, var imageFile) async {
+  final String url = '${GlobalConfiguration().getValue('base_url')}mycoverpictureedit';
+
+  print('Add chapter controller baseurl  $url');
+
+  var request = http.MultipartRequest('POST', Uri.parse(url));
+  request.fields['id'] = userId;
+  request.files.add(
+      await http.MultipartFile.fromPath('item_image', imageFile.toString()));
+  var response = await request.send();
+  print('Response stream: ${response.stream}');
+  print('Response status code: ${response.statusCode}');
+  final res = await http.Response.fromStream(response);
+  print('Response body: ${res.body}');
+
+  if(response == 200){
+    return responseModelFromJson(res.body);
+  }else{
+    return responseModelFromJson(res.body);
+  }
 }
